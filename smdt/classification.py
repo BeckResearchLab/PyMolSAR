7e38a8bf53b8a6b4aa7be993f6ad58e34df7b0cc
Rahul Avadhoot <rahulavd@uw.edu>
2018-02-19 13:25:14 -0800

    Added MLP Classifier

--- smdt/classification.py
+++ smdt/classification.py
@@ -1,173 +1,358 @@
-from sklearn import datasets
-from sklearn.preprocessing import StandardScaler, LabelEncoder, Imputer
+from sklearn.preprocessing import StandardScaler, Imputer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.feature_selection import SelectKBest
-from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import LinearSVC
-from sklearn.linear_model import SGDClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from smdt import data_processing
-from smdt import molecular_descriptors
-from sklearn import metrics
-
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score, recall_score, make_scorer, classification_report, roc_curve, precision_recall_curve, precision_score, recall_score, accuracy_score, matthews_corrcoef, jaccard_similarity_score, zero_one_loss, auc, roc_auc_score
+from smdt import metrics
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
 
-def fit_RandomForestClassifier(X_train, X_test, y_train, y_test, n_features):
+from imblearn.over_sampling import RandomOverSampler
 
-    # Standardization
-    a = StandardScaler()
-    X_train = a.fit_transform(X_train)
-    X_test = a.transform(X_test)
 
-    # Feature Selection
-    b = SelectKBest(k=n_features)
-    X_train = b.fit_transform(X_train, y_train)
-    X_test = b.transform(X_test)
+def get_modelName(clf):
+    model_name = []
+    for i in str(clf):
+        if i == '(':
+            break
+        model_name += i
+    model_name = "".join(model_name)
+    return model_name
+
+
+def fit_RandomForestClassifier(X_train, X_test, y_train, y_test, target_label):
+    # Pipeline
+    a = Imputer(missing_values='NaN', strategy='median', axis=0)
+    b = StandardScaler()
+    c = SelectKBest()
+    d = RandomOverSampler()
+    X_res, y_res = d.fit_sample(X_train, y_train)
+    clf = RandomForestClassifier()
+    model = Pipeline([('impute', a), ('scaling', b), ('anova', c), ('rf', clf)])
 
     # Grid Search CV
-    clf = RandomForestClassifier()
-    parameters = {'n_estimators': [10, 100], 'criterion': ['gini', 'entropy'],
-                  'max_features': ['auto', 'sqrt', 'log2'], 'oob_score': [True, False], 'verbose': [0],
-                  'class_weight': ['balanced',None]}
-    grid = GridSearchCV(clf, parameters, cv=10, scoring='f1_weighted')
-    grid.fit(X_train, y_train)
+    parameters = {'anova__k': [5,10,20,40],
+                  'rf__n_estimators': [10, 50], 'rf__criterion': ['gini', 'entropy'],
+                  'rf__max_features': ['auto', 'sqrt']}
+    grid = GridSearchCV(model, parameters, cv=10, scoring='f1_weighted')
+    grid.fit(X_res, y_res)
+
+    # Features Used
+    final_pipeline = grid.best_estimator_
+    select_indices = final_pipeline.named_steps['anova'].transform(np.arange(X_train.shape[1]).reshape(1, -1))
+    feature_names = X_train.columns[select_indices]
+    # Predicting and scoring on test set
+    class_index = list(grid.classes_).index(target_label)
     y_pred = grid.predict(X_test)
-    y_score = grid.predict_proba(X_test)
+    y_score = grid.predict_proba(X_test)[:, class_index]
+    model_name = get_modelName(clf)
 
+    # Plot
+    fig = plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    # ROC curve
+    metrics.plot_roc(y_test, y_score, target_label, model_name)
+    # Precision Recall Curve
+    plt.subplot(122)
+    metrics.plot_prc(y_test, y_score, target_label, model_name)
+    plt.show()
 
-    # Metrics
-    print('Training data GridSearchCV accuracy: %.5f' % grid.best_score_)
-    print('Testing Data Classification accuracy: %.5f' % grid.score(X_test, y_test))
+    # Get Metrics
+    metric = metrics.get_ClassificationMetrics(model_name, y_test, y_pred, y_score, target_label)
+    print('Training data best accuracy: %.5f' % grid.best_score_)
+    print('Testing data accuracy: %.5f' % grid.score(X_test, y_test))
     print()
     print('Classification Report:')
-    print(metrics.classification_report(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-    return grid, y_pred, y_score
-
-
-def fit_LinearSVC(X_train, X_test, y_train, y_test, n_features):
-
-    # Standardization
-    a = StandardScaler()
-    X_train = a.fit_transform(X_train)
-    X_test = a.transform(X_test)
-
-    # Feature Selection
-    b = SelectKBest(k=n_features)
-    X_train = b.fit_transform(X_train, y_train)
-    X_test = b.transform(X_test)
-    features = b.get_support(indices=True)
+    return metric, feature_names, y_score
 
 
-    # Grid Search CV
-    clf = LinearSVC()
+def fit_LinearSVC(X_train, X_test, y_train, y_test, target_label):
+
+    y_train = y_train.map({target_label: 1})
+    y_train[y_train != 1] = 0
+    y_test = y_test.map({target_label: 1})
+    y_test[y_test != 1] = 0
+    target_label = 1
+
     parameters = [{'C': [1, 10, 100], 'penalty': ['l1', 'l2'], 'dual': [False], 'loss': ['squared_hinge'], 'class_weight': ['balanced']},
                   {'C': [1, 10, 100], 'penalty': ['l2'], 'dual': [True], 'loss': ['hinge'], 'class_weight': ['balanced']}]
-    grid = GridSearchCV(clf, parameters, cv=10, scoring='f1_weighted')
-    grid.fit(X_train, y_train)
-    y_pred = grid.predict(X_test)
-    y_score = grid.decision_function(X_test)
 
-    # Metrics
-    print('Training data GridSearchCV accuracy: %.5f' % grid.best_score_)
-    print('Testing Data Classification accuracy: %.5f' % grid.score(X_test, y_test))
-    print()
-    print('Classification Report:')
-    print(metrics.classification_report(y_test, y_pred))
-
-    return grid, y_pred, y_score
-
-
-def fit_GaussianNB(X_train, X_test, y_train, y_test, n_features):
-
-    # Standardization
-    a = StandardScaler()
-    X_train = a.fit_transform(X_train)
-    X_test = a.transform(X_test)
-
-    # Feature Selection
-    b = SelectKBest(k=n_features)
-    X_train = b.fit_transform(X_train, y_train)
-    X_test = b.transform(X_test)
+    # Pipeline
+    a = Imputer(missing_values='NaN', strategy='median', axis=0)
+    b = StandardScaler()
+    c = SelectKBest()
+    d = RandomOverSampler()
+    X_res, y_res = d.fit_sample(X_train, y_train)
+    clf = LinearSVC()
+    model = Pipeline([('impute', a), ('scaling', b), ('anova', c), ('rf', clf)])
 
     # Grid Search CV
+    parameters = [{'anova__k': [5,10,20,40], 'rf__C': [1, 10, 100], 'rf__penalty': ['l1', 'l2'], 'rf__dual': [False], 'rf__loss': ['squared_hinge'],
+                   'rf__class_weight': ['balanced']},
+                  {'anova__k': [5,10,20,40], 'rf__C': [1, 10, 100], 'rf__penalty': ['l2'], 'rf__dual': [True], 'rf__loss': ['hinge'],
+                   'rf__class_weight': ['balanced']}]
+
+    grid = GridSearchCV(model, parameters, cv=10, scoring='f1_weighted')
+    grid.fit(X_res, y_res)
+
+    # Features Used
+    final_pipeline = grid.best_estimator_
+    select_indices = final_pipeline.named_steps['anova'].transform(np.arange(X_train.shape[1]).reshape(1, -1))
+    feature_names = X_train.columns[select_indices]
+    # Predicting and scoring on test set
+    y_pred = grid.predict(X_test)
+    y_score = grid.decision_function(X_test)
+    model_name = get_modelName(clf)
+
+    # Plot
+    fig = plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    # ROC curve
+    metrics.plot_roc(y_test, y_score, grid.classes_[1], model_name)
+    # Precision Recall Curve
+    plt.subplot(122)
+    metrics.plot_prc(y_test, y_score, grid.classes_[1], model_name)
+    plt.show()
+
+    # Get Metrics
+    metric = metrics.get_ClassificationMetrics(model_name, y_test, y_pred, y_score, grid.classes_[1])
+    print('Training data best accuracy: %.5f' % grid.best_score_)
+    print('Testing data accuracy: %.5f' % grid.score(X_test, y_test))
+    print()
+    print('Classification Report:')
+    print(classification_report(y_test, y_pred))
+
+    return metric, feature_names, y_score
+
+
+def fit_GaussianNB(X_train, X_test, y_train, y_test, target_label):
+
+    # Pipeline
+    a = Imputer(missing_values='NaN', strategy='median', axis=0)
+    b = StandardScaler()
+    c = SelectKBest()
+    d = RandomOverSampler()
+    X_res, y_res = d.fit_sample(X_train, y_train)
     clf = GaussianNB()
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    y_score = clf.predict_proba(X_test)
-
-    # Metrics
-    print('Training data GridSearchCV accuracy: %.5f' % clf.score(X_train, y_train))
-    print('Testing Data Classification accuracy: %.5f' % clf.score(X_test, y_test))
-    print()
-    print('Classification Report:')
-    print(metrics.classification_report(y_test, y_pred))
-
-    return clf, y_pred, y_score
-
-
-def fit_KNearestNeighbors(X_train, X_test, y_train, y_test, n_features):
-
-    # Standardization
-    a = StandardScaler()
-    X_train = a.fit_transform(X_train)
-    X_test = a.transform(X_test)
-
-    # Feature Selection
-    b = SelectKBest(k=n_features)
-    X_train = b.fit_transform(X_train, y_train)
-    X_test = b.transform(X_test)
+    model = Pipeline([('impute', a), ('scaling', b), ('anova', c), ('rf', clf)])
 
     # Grid Search CV
-    clf = KNeighborsClassifier()
+    parameters = {'anova__k': [5,10,20,40]}
+    grid = GridSearchCV(model, parameters, cv=10, scoring='f1_weighted')
+    grid.fit(X_res, y_res)
 
-    parameters = [{'n_neighbors': [5, 8, 10], 'weights': ['uniform', 'distance'], 'p': [1, 2],
-                   'algorithm': ['auto', 'ball_tree', 'kd_tree']}]
-    grid = GridSearchCV(clf, parameters, cv=10, scoring='f1_weighted')
-    grid.fit(X_train, y_train)
+    # Features Used
+    final_pipeline = grid.best_estimator_
+    select_indices = final_pipeline.named_steps['anova'].transform(np.arange(X_train.shape[1]).reshape(1, -1))
+    feature_names = X_train.columns[select_indices]
+    # Predicting and scoring on test set
+    class_index = list(grid.classes_).index(target_label)
     y_pred = grid.predict(X_test)
-    y_score = grid.predict_proba(X_test)
+    y_score = grid.predict_proba(X_test)[:, class_index]
+    model_name = get_modelName(clf)
 
+    # Plot
+    fig = plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    # ROC curve
+    metrics.plot_roc(y_test, y_score, target_label, model_name)
+    # Precision Recall Curve
+    plt.subplot(122)
+    metrics.plot_prc(y_test, y_score, target_label, model_name)
+    plt.show()
 
-    # Metrics
-    print('Training data GridSearchCV accuracy: %.5f' % grid.best_score_)
-    print('Testing Data Classification accuracy: %.5f' % grid.score(X_test, y_test))
+    # Get Metrics
+    metric = metrics.get_ClassificationMetrics(model_name, y_test, y_pred, y_score, target_label)
+    print('Training data best accuracy: %.5f' % grid.best_score_)
+    print('Testing data accuracy: %.5f' % grid.score(X_test, y_test))
     print()
     print('Classification Report:')
-    print(metrics.classification_report(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-    return grid, y_pred, y_score
+    return metric, feature_names, y_score
 
 
-def fit_SGDClassifier(X_train, X_test, y_train, y_test, n_features):
-
-    # Standardization
-    a = StandardScaler()
-    X_train = a.fit_transform(X_train)
-    X_test = a.transform(X_test)
-
-    # Feature Selection
-    b = SelectKBest(k=n_features)
-    X_train = b.fit_transform(X_train, y_train)
-    X_test = b.transform(X_test)
+def fit_KNearestNeighbors(X_train, X_test, y_train, y_test, target_label):
+    # Pipeline
+    a = Imputer(missing_values='NaN', strategy='median', axis=0)
+    b = StandardScaler()
+    c = SelectKBest()
+    d = RandomOverSampler()
+    X_res, y_res = d.fit_sample(X_train, y_train)
+    clf = KNeighborsClassifier()
+    model = Pipeline([('impute', a), ('scaling', b), ('anova', c), ('rf', clf)])
 
     # Grid Search CV
-    clf = SGDClassifier()
+    parameters = {'anova__k': [5,10,20,40],
+                  'rf__n_neighbors': [5, 8, 10], 'rf__weights': ['uniform', 'distance'], 'rf__p': [1, 2],
+                  'rf__algorithm': ['auto', 'ball_tree', 'kd_tree']}
+    grid = GridSearchCV(model, parameters, cv=10, scoring='f1_weighted')
+    grid.fit(X_res, y_res)
 
-    parameters = [
-        {'loss': ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron'], 'alpha': [0.0001, 0.00001], 'class_weight': ['balanced']}]
-    grid = GridSearchCV(clf, parameters, cv=10, scoring='f1_weighted')
-    grid.fit(X_train, y_train)
+    # Features Used
+    final_pipeline = grid.best_estimator_
+    select_indices = final_pipeline.named_steps['anova'].transform(np.arange(X_train.shape[1]).reshape(1, -1))
+    feature_names = X_train.columns[select_indices]
+    # Predicting and scoring on test set
+    class_index = list(grid.classes_).index(target_label)
+    y_pred = grid.predict(X_test)
+    y_score = grid.predict_proba(X_test)[:, class_index]
+    model_name = get_modelName(clf)
+
+    # Plot
+    fig = plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    # ROC curve
+    metrics.plot_roc(y_test, y_score, target_label, model_name)
+    # Precision Recall Curve
+    plt.subplot(122)
+    metrics.plot_prc(y_test, y_score, target_label, model_name)
+    plt.show()
+
+    # Get Metrics
+    metric = metrics.get_ClassificationMetrics(model_name, y_test, y_pred, y_score, target_label)
+    print('Training data best accuracy: %.5f' % grid.best_score_)
+    print('Testing data accuracy: %.5f' % grid.score(X_test, y_test))
+    print()
+    print('Classification Report:')
+    print(classification_report(y_test, y_pred))
+
+    return metric, feature_names, y_score
+
+
+def fit_SGDClassifier(X_train, X_test, y_train, y_test, target_label):
+
+    y_train = y_train.map({target_label: 1})
+    y_train[y_train != 1] = 0
+    y_test = y_test.map({target_label: 1})
+    y_test[y_test != 1] = 0
+    target_label = 1
+
+    # Pipeline
+    a = Imputer(missing_values='NaN', strategy='median', axis=0)
+    b = StandardScaler()
+    c = SelectKBest()
+    d = RandomOverSampler()
+    X_res, y_res = d.fit_sample(X_train, y_train)
+    clf = SGDClassifier()
+    model = Pipeline([('impute', a), ('scaling', b), ('anova', c), ('rf', clf)])
+
+    # Grid Search CV
+    parameters = {'anova__k': [5,10,20,40],
+                  'rf__loss': ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron'],
+                  'rf__alpha': [0.0001, 0.00001],'rf__class_weight': ['balanced']}
+
+    grid = GridSearchCV(model, parameters, cv=10, scoring='f1_weighted')
+    grid.fit(X_res, y_res)
+
+    # Features Used
+    final_pipeline = grid.best_estimator_
+    select_indices = final_pipeline.named_steps['anova'].transform(np.arange(X_train.shape[1]).reshape(1, -1))
+    feature_names = X_train.columns[select_indices]
+    # Predicting and scoring on test set
     y_pred = grid.predict(X_test)
     y_score = grid.decision_function(X_test)
+    model_name = get_modelName(clf)
 
-    # Metrics
-    print('Training data GridSearchCV accuracy: %.5f' % grid.best_score_)
-    print('Testing Data Classification accuracy: %.5f' % grid.score(X_test, y_test))
+    # Plot
+    fig = plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    # ROC curve
+    metrics.plot_roc(y_test, y_score, grid.classes_[1], model_name)
+    # Precision Recall Curve
+    plt.subplot(122)
+    metrics.plot_prc(y_test, y_score, grid.classes_[1], model_name)
+    plt.show()
+
+    # Get Metrics
+    metric = metrics.get_ClassificationMetrics(model_name, y_test, y_pred, y_score, grid.classes_[1])
+    print('Training data best accuracy: %.5f' % grid.best_score_)
+    print('Testing data accuracy: %.5f' % grid.score(X_test, y_test))
     print()
     print('Classification Report:')
-    print(metrics.classification_report(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-    return grid, y_pred, y_score
+    return metric, feature_names, y_score
+
+
+# define baseline model
+def baseline_model(input_dim,optimizer):
+    # create model
+    model = Sequential()
+    model.add(Dense(64, input_dim=input_dim, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return model
+
+def fit_MLPClassifier(X_train, X_test, y_train, y_test, target_label):
+
+    # Pipeline
+    a = Imputer(missing_values='NaN', strategy='median', axis=0)
+    b = StandardScaler()
+    c = SelectKBest()
+    d = RandomOverSampler()
+    X_res, y_res = d.fit_sample(X_train, y_train)
+    clf = KerasClassifier(build_fn=baseline_model,verbose=0)
+    model = Pipeline([('impute', a), ('scaling', b), ('anova', c), ('rf', clf)])
+
+    # Grid Search CV
+    parameters = [{'anova__k': [5], 'rf__input_dim':[5], 'rf__epochs':[10, 50],
+                  'rf__batch_size': [10, 20], 'rf__optimizer': ['SGD', 'Adam', 'rmsprop']},
+                  {'anova__k': [10], 'rf__input_dim': [10], 'rf__epochs': [10, 50],
+                   'rf__batch_size': [10, 20], 'rf__optimizer': ['SGD', 'Adam', 'rmsprop']},
+                  {'anova__k': [20], 'rf__input_dim': [20], 'rf__epochs': [10, 50],
+                   'rf__batch_size': [10, 20], 'rf__optimizer': ['SGD', 'Adam', 'rmsprop']},
+                  {'anova__k': [40], 'rf__input_dim': [40], 'rf__epochs': [10, 50],
+                   'rf__batch_size': [10, 20], 'rf__optimizer': ['SGD', 'Adam', 'rmsprop']}
+                  ]
+
+    grid = GridSearchCV(model, parameters, cv=10, scoring='f1_weighted')
+    grid.fit(X_res, y_res)
+
+    # Features Used
+    final_pipeline = grid.best_estimator_
+    select_indices = final_pipeline.named_steps['anova'].transform(np.arange(X_train.shape[1]).reshape(1, -1))
+    feature_names = X_train.columns[select_indices]
+    # Predicting and scoring on test set
+    class_index = list(grid.classes_).index(target_label)
+    y_pred = grid.predict(X_test)
+    y_score = grid.predict_proba(X_test)[:, class_index]
+    model_name = 'MLPClassifier'
+
+    # Plot
+    fig = plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    # ROC curve
+    metrics.plot_roc(y_test, y_score, target_label, model_name)
+    # Precision Recall Curve
+    plt.subplot(122)
+    metrics.plot_prc(y_test, y_score, target_label, model_name)
+    plt.show()
+
+    # Get Metrics
+    metric = metrics.get_ClassificationMetrics(model_name, y_test, y_pred, y_score, target_label)
+    print('Training data best accuracy: %.5f' % grid.best_score_)
+    print('Testing data accuracy: %.5f' % grid.score(X_test, y_test))
+    print()
+    print('Classification Report:')
+    print(classification_report(y_test, y_pred))
+
+    return metric, feature_names, y_score
+
--- smdt/metrics.py
+++ smdt/metrics.py
@@ -1,132 +1,50 @@
 
-from sklearn.metrics import roc_curve, auc, precision_recall_curve, confusion_matrix
-from sklearn import metrics
+from sklearn.metrics import f1_score, recall_score, make_scorer, classification_report, roc_curve, precision_recall_curve, precision_score, recall_score, accuracy_score, matthews_corrcoef, jaccard_similarity_score, zero_one_loss, auc, roc_auc_score
+from sklearn.feature_selection import f_classif, mutual_info_classif
 import matplotlib.pyplot as plt
 import itertools
-import numpy as np
 import pandas as pd
+import numpy as np
 
-def get_classifier_metrics(models, y_test, y_pred, y_score, positive_label):
-    """
 
-    """
-    fpr = []
-    tpr = []
-    roc_auc = []
-    hamming = []
-    accuracy = []
-    f1 = []
-    f_beta1 = []
-    f_beta2 = []
-    matt_corr = []
-    jac_sim = []
-    prec = []
-    rec = []
-    z1l = []
-
-    for i in range(len(y_pred)):
-        if models[i] == 'Linear SVC' or models[i] == 'SGD Classifier':
-            fpr_, tpr_, _ = roc_curve(y_test, y_score[i], pos_label=positive_label)
-        else:
-            fpr_, tpr_, _ = roc_curve(y_test, y_score[i][:, 1], pos_label=positive_label)
-        fpr.append(fpr_)
-        tpr.append(tpr_)
-        roc_auc.append(auc(fpr_, tpr_))
-        f1.append(metrics.f1_score(y_test, y_pred[i], pos_label=positive_label))
-        prec.append(metrics.precision_score(y_test, y_pred[i], pos_label=positive_label))
-        rec.append(metrics.recall_score(y_test, y_pred[i], pos_label=positive_label))
-
-        accuracy.append(metrics.accuracy_score(y_test, y_pred[i]))
-        hamming.append(metrics.hamming_loss(y_test, y_pred[i]))
-        matt_corr.append(metrics.matthews_corrcoef(y_test, y_pred[i]))
-        jac_sim.append(metrics.jaccard_similarity_score(y_test, y_pred[i]))
-        z1l.append(metrics.zero_one_loss(y_test, y_pred[i]))
-
-    a = pd.DataFrame.from_dict({'Models': models, 'Accuracy': accuracy, 'Area under ROC': roc_auc, 'Precision': prec,
-                                'Recall': rec, 'F1 Score': f1, 'Hamming Loss': hamming,
-                                'Matthews Correlation': matt_corr, 'Jaccard Similarity': jac_sim,
-                                'Zero One Loss': z1l, }, orient='index').T
-
-    fig = plt.figure(figsize=(15, 5))
-    plt.subplot(121)
-    lw = 2
-    for i in range(len(fpr)):
-        plt.plot(fpr[i], tpr[i], lw=lw, label='%s (area = %0.2f)' % (models[i], roc_auc[i]))
-
-    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+def plot_roc(y_test, y_score, target_label, model_name):
+    fpr_, tpr_, _ = roc_curve(y_test, y_score, pos_label=target_label)
+    plt.plot(fpr_, tpr_, lw=2, label='%s (area = %0.2f)' % (model_name, auc(fpr_,tpr_)))
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic')
     plt.legend(loc="lower right")
+    return
 
-    plt.subplot(122)
-    for i in range(len(fpr)):
-        if models[i] == 'Linear SVC' or models[i] == 'SGD Classifier':
-            precision, recall, _ = precision_recall_curve(y_test, y_score[i], pos_label=positive_label)
-        else:
-            precision, recall, _ = precision_recall_curve(y_test, y_score[i][:, 1], pos_label=positive_label)
-        plt.step(recall, precision, label=models[i], where='post')
+def plot_prc(y_test, y_score, target_label, model_name):
+    precision, recall, _ = precision_recall_curve(y_test, y_score, pos_label=target_label)
+    plt.step(recall, precision, label=model_name, where='post')
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.0])
     plt.title('Precision-Recall Curve')
     plt.legend(loc="lower right")
-
-    cnf_matrix = []
-    # Compute confusion matrix
-    for i in range(len(y_pred)):
-        cnf_matrix.append(confusion_matrix(y_test, y_pred[i]))
-
-    np.set_printoptions(precision=2)
-
-    plt.figure(figsize=(12, 10))
-    for i in range(len(models)):
-        plt.subplot(100 + len(models) * 10 + 1 + i)
-        plot_confusion_matrix(cnf_matrix[i], classes=y_test.unique(),
-                              title=models[i])
-
-    plt.show()
-
-    return a
+    return
 
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+def get_ClassificationMetrics(model_name, y_test, y_pred, y_score, target_label):
+    # Metrics
+    fpr_, tpr_, _ = roc_curve(y_test, y_score, pos_label=target_label)
+    f1 = f1_score(y_test, y_pred, pos_label=target_label)
+    prec = precision_score(y_test, y_pred, pos_label=target_label)
+    rec = recall_score(y_test, y_pred, pos_label=target_label)
+    accuracy = accuracy_score(y_test, y_pred)
+    matt_corr = matthews_corrcoef(y_test, y_pred)
+    jac_sim = jaccard_similarity_score(y_test, y_pred)
+    z1l = zero_one_loss(y_test, y_pred)
+    roc_auc = auc(fpr_, tpr_)
 
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
-def plot_parity(models, y_pred, y_test):
-
-    plt.figure(figsize=(15,3))
-    for i in range(len(models)):
-        plt.subplot(100+len(models)*10+1+i)
-        plt.scatter(y_test, y_pred[i], edgecolors=(0, 0, 0), alpha=0.5)
-        plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()],'r--', lw=4)
-        plt.xlabel('Measured')
-        plt.ylabel('Predicted')
-        plt.title('Parity Plot for %s'%models[i])
-
-    plt.show()
+    metric = pd.DataFrame.from_dict(
+        {'Model': model_name, 'Accuracy': accuracy, 'Area under ROC': roc_auc, 'Precision': prec,
+         'Recall': rec, 'F1 Score': f1, 'Matthews Correlation': matt_corr, 'Jaccard Similarity': jac_sim,
+         'Zero One Loss': z1l}, orient='index').T
+    return metric
