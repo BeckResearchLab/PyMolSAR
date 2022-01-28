846aa819a0d08aa77716bce1cb474c182e94c352
Keshav Dial <keshav.dial@gmail.com>
2022-01-27 22:57:50 -0500

    Update kappa.py

--- smdt/descriptors/kappa.py
+++ smdt/descriptors/kappa.py
@@ -1,7 +1,7 @@
 
 from rdkit import Chem
 from rdkit.Chem import rdchem
-from rdkit.Chem import pyPeriodicTable as PeriodicTable
+from rdkit.Chem import PeriodicTable
 periodicTable = rdchem.GetPeriodicTable()
 import pandas as pd
 
